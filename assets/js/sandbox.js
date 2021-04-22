5a6b6c16478dcc5a1bb33ea875424c5622a0b28a
DovzhenkoDenys1989 <ddv10jan1989@gmail.com>
2021-04-22 21:11:32 +0300

    Add removeEventListener

--- assets/js/sandbox.js
+++ assets/js/sandbox.js
@@ -1,38 +1,9 @@
-/*
-function makeCounter() {
-  let count = 0;
-  /* return function counter() {
-    return ++count;
-  }*/
- /* return {
-    increment() {
-      return ++count;
-    },
-    decrement() {
-      return --count;
-    },
-  };
+const btn = document.querySelector('#unique');
+
+const btnHandler = ()=>{
+  alert('success');
+  btn.removeEventListener('click', btnHandler);
 }
 
-const counter1 = makeCounter();
-const counter2 = makeCounter();*/
-
-/*
-Напишите функцию createAdder(n),
-которая вернёт другую функцию.
-Возвращенная функция должна складывать 
-получаемый аргумент m с аргументом n возвращающей функции.
-const adder = createAdder(10);
-adder(10) //20
-adder(50) //70
-adder(100) //170
-*/
-
-const createAdder = (n) => (m) => (n += m);
-
-const adder = createAdder(10);
-
-console.log(adder(20));
-console.log(adder(20));
-
+btn.addEventListener('click', btnHandler);
 
