82f6bbe275900f9c80212729969622b8f56b3067
DovzhenkoDenys1989 <ddv10jan1989@gmail.com>
2021-05-07 10:25:30 +0300

    Add createLinks

--- assets/js/solution.js
+++ assets/js/solution.js
@@ -2,3 +2,15 @@
 
 const url = new URL('https://www.instagram.com/therock/?hl=ru');
 //console.log(url.hostname);
+
+function createLinks(contacts) {
+  return contacts.map((contact) => {
+    const url = new URL(contact);
+    return createElement("a", {
+      attributes: { href: contact },
+      classNames: [SOCIAL_ICONS_MAP.get(url.hostname) || "fa-adn", "fab", 'socialLink'],
+    });
+  });
+}
+
+document.body.append(...createLinks(["https://twitter.com/realjstatham"]));
